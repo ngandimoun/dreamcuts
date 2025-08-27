ab4c6e5ffa3ef855e58536bc2c58112cdf5b5177
ngandimounchris <ngandimounchris65@yahoo.com>
2025-08-28 00:17:40 +0100

    DotLottie

---
+++ components/DotLottieReact.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const DotLottie = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/575f5e00-7cbb-4338-a8fb-5046403ec09d/5qh23QamzA.lottie"
+      loop
+      autoplay
+      className='w-10 h-14'
+    />
+  );
+};
+export default DotLottie;
+
